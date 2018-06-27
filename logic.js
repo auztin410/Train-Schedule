8645c8521af5780dd66972939b8b1484b7940ef9
auztin410 <auztin410@gmail.com>
2018-06-27 08:39:00 -0400

    Added jquery, js, and firebase references

---
+++ logic.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyCOIzQEmbMchMzLkdq8_qjMeYhR1jDT2XM",
+    authDomain: "train-schedule-8b252.firebaseapp.com",
+    databaseURL: "https://train-schedule-8b252.firebaseio.com",
+    projectId: "train-schedule-8b252",
+    storageBucket: "",
+    messagingSenderId: "692525157761"
+  };
+  firebase.initializeApp(config);
+
+  
